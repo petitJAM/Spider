be95a746006009b3e0012fd04f7ef5c0464e1b4b
Alex Petitjean <alex@tenforwardconsulting.com>
2017-10-27 19:47:48 -0500

    add headers inputs, toolbar controls for forward/back

---
+++ app/src/main/java/com/alexpetitjean/spider/data/HttpHeader.kt
@@ -0,0 +1,3 @@
+package com.alexpetitjean.spider.data
+
+data class HttpHeader(val name: String, val value: String)
---
+++ app/src/main/java/com/alexpetitjean/spider/extensions/LinearLayout.kt
@@ -0,0 +1,10 @@
+package com.alexpetitjean.spider.extensions
+
+import android.view.View
+import android.widget.LinearLayout
+
+val LinearLayout.children: List<View>
+    get () = (0 until childCount).map { i -> getChildAt(i) }
+
+
+
